e5211be1de3005ee283e7531cbb6b313ef995c57
Mohammad Khafas <mohammadkhaffas@gmail.com>
2023-02-06 09:44:04 +0000

    formatting my codes

--- app.js
+++ app.js
@@ -51,16 +51,16 @@ function getQuote() {
   $("#tweet-quote").attr(
     "href",
     "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
-      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+    encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
   );
 
   $("#tumblr-quote").attr(
     "href",
     "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=" +
-      encodeURIComponent(currentAuthor) +
-      "&content=" +
-      encodeURIComponent(currentQuote) +
-      "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
+    encodeURIComponent(currentAuthor) +
+    "&content=" +
+    encodeURIComponent(currentQuote) +
+    "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
   );
 
   $(".quote-text").animate({ opacity: 0 }, 500, function () {
